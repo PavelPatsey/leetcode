cac1523e5d891d34fe46d2e44426800faf16a58e
Павел <patsey.pavel@gmail.com>
2023-12-30 23:44:12 +0300

    solved easy 0001 in Elixir

--- elixir/easy/0001/0001.exs
+++ elixir/easy/0001/0001.exs
@@ -1,12 +1,16 @@
 defmodule Solution do
   @spec two_sum(nums :: [integer], target :: integer) :: [integer]
   def two_sum(nums, target) do
-    nums_indexed = Enum.with_index(nums) |> IO.inspect()
+    nums_indexed = Enum.with_index(nums)
 
-    for x <- nums_indexed,
-        y <- nums_indexed,
-        elem(x, 0) + elem(y, 0) == target |> IO.inspect(),
-        do: [elem(x, 1), elem(y, 1)]
+    result =
+      for x <- nums_indexed,
+          y <- nums_indexed,
+          elem(x, 1) < elem(y, 1) and elem(x, 0) + elem(y, 0) == target,
+          do: [elem(x, 1), elem(y, 1)]
+
+    [head | tail] = result
+    head
   end
 end
 
@@ -19,11 +23,11 @@ defmodule AssertionTest do
     assert Solution.two_sum([2, 7, 11, 15], 9) == [0, 1]
   end
 
-  # test "test_2" do
-  #   assert Solution.two_sum([3, 2, 4], [3, 2, 4]) == [1, 2]
-  # end
+  test "test_2" do
+    assert Solution.two_sum([3, 2, 4], 6) == [1, 2]
+  end
 
-  # test "test_3" do
-  #   assert Solution.two_sum([3, 3], 6) == [0, 1]
-  # end
+  test "test_3" do
+    assert Solution.two_sum([3, 3], 6) == [0, 1]
+  end
 end
