10c82e1b3b79f4cee21eb4e07fd1b9cd03087244
Павел <patsey.pavel@gmail.com>
2024-01-04 14:19:23 +0300

    solved medium 2870 in Elixir

--- elixir/medium/2870/2870.exs
+++ elixir/medium/2870/2870.exs
@@ -1,11 +1,18 @@
 defmodule Solution do
   @spec min_operations(nums :: [integer]) :: integer
   def min_operations(nums) do
-    nums
-    |> Enum.frequencies()
-    |> IO.inspect()
-    |> Map.values()
-    |> IO.inspect()
+    values =
+      nums
+      |> Enum.frequencies()
+      |> Map.values()
+
+    if 1 in values do
+      -1
+    else
+      values
+      |> Enum.map(fn x -> div(x, 3) + boolean_to_integer(rem(x, 3) != 0) end)
+      |> Enum.sum()
+    end
   end
 
   def boolean_to_integer(bool) do
@@ -13,18 +20,21 @@ defmodule Solution do
   end
 end
 
-Solution.min_operations([2, 3, 3, 2, 2, 4, 2, 3, 4])
+ExUnit.start()
 
-# ExUnit.start()
+defmodule AssertionTest do
+  use ExUnit.Case, async: true
 
-# defmodule AssertionTest do
-#   use ExUnit.Case, async: true
+  test "test_1" do
+    assert Solution.min_operations([2, 3, 3, 2, 2, 4, 2, 3, 4]) == 4
+  end
 
-#   test "test_1" do
-#     assert Solution.min_operations([2, 3, 3, 2, 2, 4, 2, 3, 4]) == 4
-#   end
+  test "test_2" do
+    assert Solution.min_operations([2, 1, 2, 2, 3, 3]) == -1
+  end
 
-#   test "test_2" do
-#     assert Solution.min_operations([2, 1, 2, 2, 3, 3]) == -1
-#   end
-# end
+  test "test_3" do
+    assert Solution.min_operations([16, 16, 16, 19, 16, 3, 16, 8, 16, 16, 16, 19, 3, 16, 16]) ==
+             -1
+  end
+end
