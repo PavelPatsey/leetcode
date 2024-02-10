0f7fd4264052870d5c695406f9cf148683bddfbe
Павел Пацей <patsey.pavel@gmail.com>
2024-02-10 14:11:04 +0300

    refactored

--- py/medium/0647/0647_dp_o(n2).py
+++ py/medium/0647/0647_dp_o(n2).py
@@ -1,20 +1,23 @@
 class Solution:
+    @staticmethod
+    def get_count(s, left, right):
+        count = 0
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            count += 1
+            left -= 1
+            right += 1
+        return count
+
     def countSubstrings(self, s: str) -> int:
         count = 0
         len_s = len(s)
         for i in range(len_s):
             left = right = i
-            while left >= 0 and right < len_s and s[left] == s[right]:
-                count += 1
-                left -= 1
-                right += 1
+            count += self.get_count(s, left, right)
 
             left = i
             right = i + 1
-            while left >= 0 and right < len_s and s[left] == s[right]:
-                count += 1
-                left -= 1
-                right += 1
+            count += self.get_count(s, left, right)
         return count
 
 
