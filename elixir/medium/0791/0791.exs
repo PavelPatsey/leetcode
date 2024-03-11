235cb10bb04d3c8b5b3c6b671503c6d22fdc57c1
Павел Пацей <patsey.pavel@gmail.com>
2024-03-11 22:51:14 +0300

    added and teted get_index

---
+++ elixir/medium/0791/0791.exs
@@ -0,0 +1,43 @@
+defmodule Solution do
+  @spec custom_sort_string(order :: String.t(), s :: String.t()) :: String.t()
+  def custom_sort_string(order, s) do
+    s
+    |> String.graphemes()
+    |> IO.inspect()
+    |> Enum.sort(&get_index(&1, order))
+    |> IO.inspect()
+  end
+
+  def get_index(char, order) do
+    do_get_index(char, order, 0)
+  end
+
+  defp do_get_index(_char, [], _acc) do
+    -1
+  end
+
+  defp do_get_index(char, [char | _tail], acc) do
+    acc
+  end
+
+  defp do_get_index(char, [_head | tail], acc) do
+    do_get_index(char, tail, acc + 1)
+  end
+end
+
+ExUnit.start()
+
+defmodule AssertionTest do
+  use ExUnit.Case, async: true
+
+  test "test get_index" do
+    assert Solution.get_index("a", String.graphemes("abcd")) == 0
+    assert Solution.get_index("b", String.graphemes("abcd")) == 1
+    assert Solution.get_index("d", String.graphemes("abcd")) == 3
+    assert Solution.get_index("z", String.graphemes("abcd")) == -1
+  end
+
+  test "test custom_sort_string" do
+    assert Solution.custom_sort_string("cba", "abcd") == "dcba"
+  end
+end
