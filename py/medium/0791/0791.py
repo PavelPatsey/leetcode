ff282e014848e5a425321b0e0f484cab3f30731f
Павел Пацей <patsey.pavel@gmail.com>
2024-03-11 22:22:02 +0300

    refactored

--- py/medium/0791/0791.py
+++ py/medium/0791/0791.py
@@ -1,9 +1,6 @@
 class Solution:
     def customSortString(self, order: str, s: str) -> str:
-        mapped = map(lambda x: (x, order.find(x)), s)
-        _sorted = sorted(mapped, key=lambda x: x[1])
-        mapped = map(lambda x: x[0], _sorted)
-        return "".join(mapped)
+        return "".join(sorted(s, key=order.find))
 
 
 solution = Solution()
