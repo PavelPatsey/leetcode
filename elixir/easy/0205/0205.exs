04bab2699f5087558bd70390f670f307cb8825e7
Павел <patsey.pavel@gmail.com>
2024-04-02 19:13:56 +0300

    solved easy 0205 in Elixir

---
+++ elixir/easy/0205/0205.exs
@@ -0,0 +1,38 @@
+defmodule Solution do
+  @spec is_isomorphic(s :: String.t(), t :: String.t()) :: boolean
+  def is_isomorphic(s, t) do
+    size_s =
+      s
+      |> String.to_charlist()
+      |> MapSet.new()
+      |> MapSet.size()
+
+    size_t =
+      t
+      |> String.to_charlist()
+      |> MapSet.new()
+      |> MapSet.size()
+
+    size_zip =
+      [s, t]
+      |> Enum.map(&String.to_charlist(&1))
+      |> Enum.zip()
+      |> MapSet.new()
+      |> MapSet.size()
+
+    size_s == size_t and size_t == size_zip
+  end
+end
+
+ExUnit.start()
+
+defmodule AssertionTest do
+  use ExUnit.Case, async: true
+
+  test "is_isomorphic" do
+    assert Solution.is_isomorphic("egg", "add") == true
+    assert Solution.is_isomorphic("foo", "bar") == false
+    assert Solution.is_isomorphic("paper", "title") == true
+    assert Solution.is_isomorphic("badc", "baba") == false
+  end
+end
