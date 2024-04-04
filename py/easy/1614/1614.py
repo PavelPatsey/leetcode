b8bf12bb151342c49d9c5dad3aeba9fe69a0faa0
Павел <patsey.pavel@gmail.com>
2024-04-04 18:41:49 +0300

    solved easy 16135 in Python

---
+++ py/easy/1614/1614.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxDepth(self, s: str) -> int:
+        current_counter = max_counter = 0
+        for char in s:
+            if char == "(":
+                current_counter += 1
+            elif char == ")":
+                current_counter -= 1
+            max_counter = max(max_counter, current_counter)
+
+        return max_counter
+
+
+solution = Solution()
+assert solution.maxDepth("(1+(2*3)+((8)/4))+1") == 3
+assert solution.maxDepth("(1)+((2))+(((3)))") == 3
