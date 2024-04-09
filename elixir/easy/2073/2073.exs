5888f538584dafd932be22c4ab0ba4c89e90fea3
Павел <patsey.pavel@gmail.com>
2024-04-09 23:59:18 +0300

    solved easy 2073 in Elixir

---
+++ elixir/easy/2073/2073.exs
@@ -0,0 +1,31 @@
+defmodule Solution do
+  @spec time_required_to_buy(tickets :: [integer], k :: integer) :: integer
+  def time_required_to_buy(tickets, k) do
+    k_ticket = Enum.at(tickets, k)
+    do_time_required_to_buy(tickets, 0, k, k_ticket, 0)
+  end
+
+  defp do_time_required_to_buy([], _i, _k, _k_ticket, acc), do: acc
+
+  defp do_time_required_to_buy([head | tail], i, k, k_ticket, acc) do
+    if i <= k do
+      new_acc = acc + min(head, k_ticket)
+      do_time_required_to_buy(tail, i + 1, k, k_ticket, new_acc)
+    else
+      new_acc = acc + min(head, k_ticket - 1)
+      do_time_required_to_buy(tail, i + 1, k, k_ticket, new_acc)
+    end
+  end
+end
+
+ExUnit.start()
+
+defmodule AssertionTest do
+  use ExUnit.Case, async: true
+
+  test "test time_required_to_buy" do
+    assert Solution.time_required_to_buy([2, 3, 2], 2) == 6
+    assert Solution.time_required_to_buy([5, 1, 1, 1], 0) == 8
+    assert Solution.time_required_to_buy([84, 49, 5, 24, 70, 77, 87, 8], 3) == 154
+  end
+end
