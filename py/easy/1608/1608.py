d1852ed1eefec5102328f36baf8c066e26867fc9
Павел Пацей <patsey.pavel@gmail.com>
2024-05-27 21:51:40 +0300

    solved easy 1608 in Python

---
+++ py/easy/1608/1608.py
@@ -0,0 +1,24 @@
+from typing import List
+
+
+class Solution:
+    def specialArray(self, nums: List[int]) -> int:
+        len_nums = len(nums)
+        x = 0
+        while x < len_nums:
+            i = 0
+            counter = 0
+            while i < len_nums:
+                if nums[i] >= x:
+                    counter += 1
+                i += 1
+            x += 1
+            if counter == x:
+                return x
+        return -1
+
+
+solution = Solution()
+assert solution.specialArray([3, 5]) == 2
+assert solution.specialArray([0, 0]) == -1
+assert solution.specialArray([0, 4, 3, 0, 4]) == 3
