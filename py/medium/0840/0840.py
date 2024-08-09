c998fc3b6fe821770708280a4fda19433e4f31cb
Павел <patsey.pavel@gmail.com>
2024-08-09 16:24:13 +0300

    refactored

--- py/medium/0840/0840.py
+++ py/medium/0840/0840.py
@@ -4,20 +4,17 @@ from typing import List
 class Solution:
     @staticmethod
     def is_matrix_magic(matrix):
-        max_row = len(matrix)
-        max_col = len(matrix[0])
-        assert max_col == max_row == 3
+        assert len(matrix) == len(matrix[0]) == 3
 
         a = sum(matrix[0])
         b = sum(matrix[1])
         c = sum(matrix[2])
         d = matrix[0][0] + matrix[1][1] + matrix[2][2]
         e = matrix[2][0] + matrix[1][1] + matrix[0][2]
-        for s in [b, c, d, e]:
+        for s in (b, c, d, e):
             if s != a:
                 return False
-        else:
-            return True
+        return True
 
     def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
         pass
