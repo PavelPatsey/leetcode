e9543695e62f29808665f053682094650e69f7b4
Павел <patsey.pavel@gmail.com>
2024-08-09 17:03:04 +0300

    solved medium 0840 in Python

--- py/medium/0840/0840.py
+++ py/medium/0840/0840.py
@@ -3,25 +3,41 @@ from typing import List
 
 class Solution:
     @staticmethod
-    def is_matrix_magic(matrix):
-        assert len(matrix) == len(matrix[0]) == 3
+    def is_magic_matrix(matrix, i, j):
+        elements = set()
+        for r in range(i, i + 3):
+            for c in range(j, j + 3):
+                if 1 <= matrix[r][c] <= 9:
+                    elements.add(matrix[r][c])
+        if len(elements) != 9:
+            return False
 
-        a = sum(matrix[0])
-        b = sum(matrix[1])
-        c = sum(matrix[2])
-        d = matrix[0][0] + matrix[1][1] + matrix[2][2]
-        e = matrix[2][0] + matrix[1][1] + matrix[0][2]
-        for s in (b, c, d, e):
-            if s != a:
+        a = sum(matrix[i][j : j + 3])
+        for x in range(1, 3):
+            if a != sum(matrix[i + x][j : j + 3]):
                 return False
+        for x in range(3):
+            if a != matrix[i][j + x] + matrix[i + 1][j + x] + matrix[i + 2][j + x]:
+                return False
+        if a != matrix[i][j] + matrix[i + 1][j + 1] + matrix[i + 2][j + 2]:
+            return False
+        if a != matrix[i + 2][j] + matrix[i + 1][j + 1] + matrix[i][j + 2]:
+            return False
         return True
 
     def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
-        pass
+        row_max = len(grid)
+        col_max = len(grid[0])
+        counter = 0
+        for row in range(row_max - 2):
+            for col in range(col_max - 2):
+                if self.is_magic_matrix(grid, row, col):
+                    counter += 1
+        return counter
 
 
 solution = Solution()
-assert solution.is_matrix_magic([[4, 3, 8], [9, 5, 1], [2, 7, 6]]) is True
-assert solution.is_matrix_magic([[1, 2, 3], [1, 1, 1], [1, 1, 1]]) is False
-
 assert solution.numMagicSquaresInside([[4, 3, 8, 4], [9, 5, 1, 9], [2, 7, 6, 2]]) == 1
+assert solution.numMagicSquaresInside([[8]]) == 0
+assert solution.numMagicSquaresInside([[2, 7, 6], [1, 5, 9], [4, 3, 8]]) == 0
+assert solution.numMagicSquaresInside([[5, 5, 5], [5, 5, 5], [5, 5, 5]]) == 0
