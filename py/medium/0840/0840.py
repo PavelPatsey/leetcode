66d0acb71ff195cf6f78ba85f8c5561b83a85630
Павел <patsey.pavel@gmail.com>
2024-08-09 16:23:04 +0300

    added is_matrix_magic method

---
+++ py/medium/0840/0840.py
@@ -0,0 +1,30 @@
+from typing import List
+
+
+class Solution:
+    @staticmethod
+    def is_matrix_magic(matrix):
+        max_row = len(matrix)
+        max_col = len(matrix[0])
+        assert max_col == max_row == 3
+
+        a = sum(matrix[0])
+        b = sum(matrix[1])
+        c = sum(matrix[2])
+        d = matrix[0][0] + matrix[1][1] + matrix[2][2]
+        e = matrix[2][0] + matrix[1][1] + matrix[0][2]
+        for s in [b, c, d, e]:
+            if s != a:
+                return False
+        else:
+            return True
+
+    def numMagicSquaresInside(self, grid: List[List[int]]) -> int:
+        pass
+
+
+solution = Solution()
+assert solution.is_matrix_magic([[4, 3, 8], [9, 5, 1], [2, 7, 6]]) is True
+assert solution.is_matrix_magic([[1, 2, 3], [1, 1, 1], [1, 1, 1]]) is False
+
+assert solution.numMagicSquaresInside([[4, 3, 8, 4], [9, 5, 1, 9], [2, 7, 6, 2]]) == 1
