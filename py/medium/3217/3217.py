c2d7de33fe7e579cc447fcc9f158a0e670d3ff43
Павел <patsey.pavel@gmail.com>
2024-09-06 18:11:57 +0300

    solved medium 3217 in Python

--- py/medium/3217/3217.py
+++ py/medium/3217/3217.py
@@ -7,29 +7,53 @@ class ListNode:
         self.val = val
         self.next = next
 
-
-class Solution:
-    @staticmethod
-    def _traversal(node: ListNode):
+    def traversal(self):
         result = []
-        while node.next:
+        node = self
+        while node:
             result.append(node.val)
             node = node.next
         return result
 
+
+class Solution:
     def modifiedList(
-            self, nums: List[int], head: Optional[ListNode]
+        self,
+        nums: List[int],
+        head: Optional[ListNode],
     ) -> Optional[ListNode]:
-        pass
+        nums_set = set(nums)
+        node = head
+        while node and node.val in nums_set:
+            node = node.next
+
+        if not node:
+            return None
+
+        previous = node
+        current = node.next
+        while current:
+            if current.val not in nums_set:
+                previous.next = current
+                previous = current
+            current = current.next
+        previous.next = None
+        return node
 
 
-# head = [1,2,3,4,5]
+test_head = [1, 2, 1, 2, 1, 2]
 list_node = ListNode(1)
 list_node.next = ListNode(2)
-list_node.next.next = ListNode(3)
-list_node.next.next.next = ListNode(4)
-list_node.next.next.next.next = ListNode(5)
+list_node.next.next = ListNode(1)
+list_node.next.next.next = ListNode(2)
+list_node.next.next.next.next = ListNode(1)
+list_node.next.next.next.next.next = ListNode(2)
+
+list_node_2 = ListNode(2)
+list_node_2.next = ListNode(2)
+list_node_2.next.next = ListNode(2)
 
 solution = Solution()
-solution._traversal(list_node) == [1, 2, 3, 4, 5]
-# assert solution.modifiedList([1, 2, 3], list_node) == [2, 2, 2]
+a = solution.modifiedList([1], list_node)
+
+assert solution.modifiedList([1], list_node).traversal() == list_node_2.traversal()
