9de4b415ad46cbfd986fd060ca8ea2c1013ea3d5
Павел <patsey.pavel@gmail.com>
2024-09-09 13:00:47 +0300

    added _get_list_node function

--- py/medium/2326/2326.py
+++ py/medium/2326/2326.py
@@ -16,15 +16,21 @@ class ListNode:
         return result
 
 
+def _get_list_node(head: List) -> ListNode:
+    node = current_node = ListNode(head[0])
+    for value in head[1:]:
+        current_node.next = ListNode(value)
+        current_node = current_node.next
+    return node
+
+
 class Solution:
     def spiralMatrix(self, m: int, n: int, head: Optional[ListNode]) -> List[List[int]]:
         pass
 
 
-test_head = [3, 0, 2, 6, 8, 1, 7, 9, 4, 2, 5, 5, 0]
-list_node = current_node = ListNode(test_head[0])
-for value in test_head[1:]:
-    current_node.next = ListNode(value)
-    current_node = current_node.next
-
 solution = Solution()
+
+test_head = [3, 0, 2, 6, 8, 1, 7, 9, 4, 2, 5, 5, 0]
+list_node = _get_list_node(test_head)
+assert list_node.traversal() == test_head
