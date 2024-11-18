7fbf60b0df32791d23ecd0fb073323eaf61de781
Павел Пацей <patsey.pavel@gmail.com>
2024-11-18 22:34:18 +0300

    added a faster solution

--- py/easy/1652/1652.py
+++ py/easy/1652/1652.py
@@ -3,20 +3,18 @@ from typing import List
 
 class Solution:
     def decrypt(self, code: List[int], k: int) -> List[int]:
-        result = []
         N = len(code)
-        for i in range(N):
-            s = 0
-            if k > 0:
-                for j in range(1, k + 1):
-                    s += code[(i + j) % N]
-                result.append(s)
-            elif k < 0:
-                for j in range(-1, k - 1, -1):
-                    s += code[(i + j) % N]
-                result.append(s)
-            else:
-                result.append(0)
+        if k == 0:
+            return [0] * N
+        elif k > 0:
+            ext_code = code + code
+            result = [sum(ext_code[i + 1 : i + k + 1]) for i in range(N)]
+        else:
+            rev_ext_code = (code + code)[::-1]
+            k = -k
+            result = [
+                sum(rev_ext_code[i + 1 : i + k + 1]) for i in range(N - 1, -1, -1)
+            ]
         return result
 
 
