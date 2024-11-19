deac08d857e885e549703ed1b5d2cb54f49b7720
Павел <patsey.pavel@gmail.com>
2024-11-20 00:30:33 +0300

    solved medium 2461 in Python

--- py/medium/2461/2461.py
+++ py/medium/2461/2461.py
@@ -1,21 +1,23 @@
-from collections import deque
+from collections import Counter
 from typing import List
 
 
 class Solution:
     def maximumSubarraySum(self, nums: List[int], k: int) -> int:
-        subarray = deque(nums[0:k])
-        result = sum(subarray) if len(subarray) == len(set(subarray)) else 0
-        current_sum = sum(subarray)
+        counter = Counter(nums[0:k])
+        current_sum = sum(nums[0:k])
+        result = current_sum if len(counter.keys()) == k else 0
 
         for i in range(1, len(nums) - k + 1):
-            current_sum -= subarray.popleft()
-            subarray.append(nums[i + k - 1])
-            current_sum += nums[i + k - 1]
-            result = max(
-                result,
-                current_sum if len(subarray) == len(set(subarray)) else 0,
-            )
+            for_discard = nums[i - 1]
+            counter[for_discard] -= 1
+            if counter[for_discard] == 0:
+                del counter[for_discard]
+            current_sum -= for_discard
+            for_add = nums[i + k - 1]
+            counter[for_add] += 1
+            current_sum += for_add
+            result = max(result, current_sum if len(counter.keys()) == k else 0)
         return result
 
 
@@ -23,3 +25,4 @@ solution = Solution()
 assert solution.maximumSubarraySum([1, 5, 4, 2, 9, 9, 9], 3) == 15
 assert solution.maximumSubarraySum([4, 4, 4], 3) == 0
 assert solution.maximumSubarraySum([1, 1, 1, 7, 8, 9], 3) == 24
+assert solution.maximumSubarraySum([9, 9, 9, 1, 2, 3], 3) == 12
