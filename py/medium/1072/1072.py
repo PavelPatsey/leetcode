e4b5cbde48d326b74a21b09b152a4b832d2dfc0f
Павел <patsey.pavel@gmail.com>
2024-11-23 00:20:12 +0300

    solved medium 1072 in Python

---
+++ py/medium/1072/1072.py
@@ -0,0 +1,17 @@
+from collections import Counter
+from typing import List
+
+
+class Solution:
+    def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
+        counter = Counter()
+        for row in matrix:
+            pattern = tuple(x == row[0] for x in row)
+            counter[pattern] += 1
+        return max(counter.values())
+
+
+solution = Solution()
+assert solution.maxEqualRowsAfterFlips([[0, 1], [1, 1]]) == 1
+assert solution.maxEqualRowsAfterFlips([[0, 1], [1, 0]]) == 2
+assert solution.maxEqualRowsAfterFlips([[0, 0, 0], [0, 0, 1], [1, 1, 0]]) == 2
