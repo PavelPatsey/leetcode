f3530ee0d1c9d3e4eeba949846757762d9d5c91a
Pavel Patsey <patsey.pavel@gmail.com>
2025-03-11 23:12:36 +0300

    Add medium 1358 solution in Python

--- py/medium/1358/1358.py
+++ py/medium/1358/1358.py
@@ -1,18 +1,28 @@
+import math
+
+
 class Solution:
     def numberOfSubstrings(self, s: str) -> int:
         res = 0
-        for i in range(len(s)):
-            substring = s[i:]
-            char_set = set()
-            for x, char in enumerate(substring):
-                char_set.add(char)
-                if len(char_set) == 3:
-                    d_res = len(substring) - x
-                    res += d_res
-                    break
+        left = 0
+        right = 0
+        counter = {"a": 0, "b": 0, "c": 0}
+        counter[s[left]] += 1
+        while left <= right < len(s):
+            if math.prod(counter.values()) == 0:
+                right += 1
+                if right < len(s):
+                    counter[s[right]] += 1
+            elif math.prod(counter.values()) != 0:
+                counter[s[left]] -= 1
+                left += 1
+                res += len(s) - right
+            else:
+                assert False
         return res
 
 
 solution = Solution()
 assert solution.numberOfSubstrings("abcabc") == 10
 assert solution.numberOfSubstrings("aaacb") == 3
+assert solution.numberOfSubstrings("ababbbc") == 3
