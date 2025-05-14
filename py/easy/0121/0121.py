9e416b2d8f85a2ad8353d41d974b47a996499700
Pavel Patsey <patsey.pavel@gmail.com>
2025-05-15 00:04:21 +0300

    Add easy 0121 solution in Python

--- py/easy/0121/0121.py
+++ py/easy/0121/0121.py
@@ -1,14 +1,20 @@
 class Solution:
     def maxProfit(self, prices: list[int]) -> int:
-        res = 0
-        for i in range(len(prices)):
-            first = prices[i]
-            for x in prices[i + 1 :]:
-                if x > first:
-                    res = max(res, x - first)
+        rev_prices = prices[::-1]
+        max_x = rev_prices[0]
+        diff = 0
+        res = diff
+        for x in rev_prices:
+            if x > max_x:
+                max_x = x
+                diff = 0
+            else:
+                diff = max(diff, max_x - x)
+                res = max(res, diff)
         return res
 
 
 solution = Solution()
 assert solution.maxProfit([7, 1, 5, 3, 6, 4]) == 5
 assert solution.maxProfit([7, 6, 4, 3, 1]) == 0
+assert solution.maxProfit([1, 2]) == 1
