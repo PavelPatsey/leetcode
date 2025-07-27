cca507ac983473de38efdb460f8e91eb6423dcea
Pavel Patsey <patsey.pavel@gmail.com>
2025-07-27 21:06:27 +0300

    Do refactoring

--- py/easy/2210/2210.py
+++ py/easy/2210/2210.py
@@ -4,18 +4,16 @@ class Solution:
         if not nums:
             return []
         result = [nums[0]]
-        for x, y in zip(nums, nums[1:]):
-            if x != y:
-                result.append(y)
+        for x in nums[1:]:
+            if x != result[-1]:
+                result.append(x)
         return result
 
     def countHillValley(self, nums: list[int]) -> int:
         removed = self.remove_duplicates(nums)
         counter = 0
         for x, y, z in zip(removed, removed[1:], removed[2:]):
-            if x < y > z:
-                counter += 1
-            if x > y < z:
+            if x < y > z or x > y < z:
                 counter += 1
         return counter
 
@@ -30,4 +28,4 @@ assert solution.remove_duplicates([]) == []
 
 
 assert solution.countHillValley([2, 4, 1, 1, 6, 5]) == 3
-assert solution.countHillValley([6, 6, 5, 5, 4, 1]) == 0
+# assert solution.countHillValley([6, 6, 5, 5, 4, 1]) == 0
