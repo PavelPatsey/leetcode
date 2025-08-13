ac32955cdde307b1e653006b2ecb5061a1dfb98e
Pavel Patsey <patsey.pavel@gmail.com>
2025-08-13 17:34:33 +0300

    Add easy 0326 solution in Python

---
+++ py/easy/0326/0326.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n <= 0:
+            return False
+        if n == 1:
+            return True
+        if n % 3 == 0:
+            return self.isPowerOfThree(n // 3)
+        return False
+
+
+solution = Solution()
+assert solution.isPowerOfThree(27) is True
+assert solution.isPowerOfThree(0) is False
+assert solution.isPowerOfThree(-1) is False
