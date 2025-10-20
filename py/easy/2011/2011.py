3f96ea312b79be0bd3fc808868adad5ad20c17ca
Pavel Patsey <patsey.pavel@gmail.com>
2025-10-20 17:12:14 +0300

    Add functional solutuon

--- py/easy/2011/2011.py
+++ py/easy/2011/2011.py
@@ -9,7 +9,17 @@ class Solution:
         return x
 
 
+class Solution2:
+    def finalValueAfterOperations(self, operations: list[str]) -> int:
+        return sum(map(lambda x: 1 if "+" in x else -1, operations))
+
+
 solution = Solution()
+assert solution.finalValueAfterOperations(["--X", "X++", "X++"]) == 1
+assert solution.finalValueAfterOperations(["++X", "++X", "X++"]) == 3
+assert solution.finalValueAfterOperations(["X++", "++X", "--X", "X--"]) == 0
+
+solution_2 = Solution2()
 assert solution.finalValueAfterOperations(["--X", "X++", "X++"]) == 1
 assert solution.finalValueAfterOperations(["++X", "++X", "X++"]) == 3
 assert solution.finalValueAfterOperations(["X++", "++X", "--X", "X--"]) == 0
