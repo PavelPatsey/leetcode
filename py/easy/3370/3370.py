f93e5bcba58714a8b8e5a31a44f5fc11e7d87846
Pavel Patsey <patsey.pavel@gmail.com>
2025-10-29 23:16:06 +0300

    Add a more beautiful solution

--- py/easy/3370/3370.py
+++ py/easy/3370/3370.py
@@ -1,5 +1,8 @@
 class Solution:
     def smallestNumber(self, n: int) -> int:
+        return (1 << n.bit_length()) - 1
+
+    def _smallestNumber(self, n: int) -> int:
         len_bin_n = len(bin(n)) - 2
         return int(2**len_bin_n - 1)
 
@@ -8,3 +11,7 @@ solution = Solution()
 assert solution.smallestNumber(5) == 7
 assert solution.smallestNumber(10) == 15
 assert solution.smallestNumber(3) == 3
+
+assert solution._smallestNumber(5) == 7
+assert solution._smallestNumber(10) == 15
+assert solution._smallestNumber(3) == 3
