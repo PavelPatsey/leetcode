66928757d3b2df52001c122d46b8958cd3dad96c
Pavel Patsey <patsey.pavel@gmail.com>
2025-11-02 18:50:11 +0300

    Make solution faster

--- py/medium/0003/0003.py
+++ py/medium/0003/0003.py
@@ -2,13 +2,16 @@ class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         left = right = 0
         res = 0
-        while right <= len(s):
-            current = s[left:right]
-            if len(current) == len(set(current)):
-                res = max(right - left, res)
-                right += 1
-            else:
-                left += 1
+        char_set = set()
+        while right < len(s):
+            new_char = s[right]
+            if new_char in char_set:
+                while new_char in char_set:
+                    char_set.remove(s[left])
+                    left += 1
+            char_set.add(new_char)
+            right += 1
+            res = max(res, right - left)
         return res
 
 
@@ -17,3 +20,4 @@ assert solution.lengthOfLongestSubstring("abcabcbb") == 3
 assert solution.lengthOfLongestSubstring("abc") == 3
 assert solution.lengthOfLongestSubstring("bbbbb") == 1
 assert solution.lengthOfLongestSubstring("pwwkew") == 3
+assert solution.lengthOfLongestSubstring("aab") == 2
