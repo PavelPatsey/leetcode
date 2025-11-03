29df44c2e708c34a3c002803ae8670227ec3ba7d
Pavel Patsey <patsey.pavel@gmail.com>
2025-11-03 21:50:17 +0300

    small fix

--- py/medium/0424/0424_TLE.py
+++ py/medium/0424/0424_TLE.py
@@ -3,16 +3,17 @@ from collections import Counter
 
 class Solution:
     @staticmethod
-    def needed_permutations(s: str):
+    def valid_substring(s: str, k: int):
         counter = Counter(s)
-        return sum(counter.values()) - max(counter.values())
+        replaces = sum(counter.values()) - max(counter.values())
+        return replaces <= k
 
     def characterReplacement(self, s: str, k: int) -> int:
         res = 0
         left = 0
         while left < len(s):
             right = left + 1
-            while right <= len(s) and self.needed_permutations(s[left:right]) <= k:
+            while right <= len(s) and self.valid_substring(s[left:right], k):
                 right += 1
             res = max(right - left - 1, res)
             left += 1
@@ -25,10 +26,3 @@ assert solution.characterReplacement("AABABBA", 1) == 4
 assert solution.characterReplacement("A", 0) == 1
 assert solution.characterReplacement("A", 1) == 1
 assert solution.characterReplacement("ABBB", 2) == 4
-
-
-assert solution.needed_permutations("ab") == 1
-assert solution.needed_permutations("aba") == 1
-assert solution.needed_permutations("abaa") == 1
-assert solution.needed_permutations("abc") == 2
-assert solution.needed_permutations("a") == 0
