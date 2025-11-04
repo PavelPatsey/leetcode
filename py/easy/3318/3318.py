d5ba2ddef7296222c95bfb2accdb2ecf90c8f300
Pavel Patsey <patsey.pavel@gmail.com>
2025-11-04 14:44:19 +0300

    refactor

--- py/easy/3318/3318.py
+++ py/easy/3318/3318.py
@@ -1,5 +1,5 @@
 from collections import Counter
-from functools import cmp_to_key
+from math import prod
 
 
 class Solution:
@@ -10,27 +10,11 @@ class Solution:
         return res
 
 
-def mycmp(tuple_a, tuple_b) -> int:
-    key_a, value_a = tuple_a
-    key_b, value_b = tuple_b
-    if value_a == value_b:
-        if key_a > key_b:
-            return 1
-        elif key_a < key_b:
-            return -1
-        else:
-            return 0
-    if value_a > value_b:
-        return 1
-    elif value_a < value_b:
-        return -1
-
-
 def xsum(nums: list, x: int) -> int:
     counter = Counter(nums)
     items = list(counter.items())
-    sorted_mapped = sorted(items, key=cmp_to_key(mycmp), reverse=True)
-    return sum((n[0] * n[1] for n in sorted_mapped[0:x]))
+    sorted_mapped = sorted(items, key=lambda x: (x[1], x[0]), reverse=True)
+    return sum(prod(n) for n in sorted_mapped[0:x])
 
 
 solution = Solution()
