9412fa11708761fd0eeb7e3aa0c5e7bb7f9c274f
Pavel Patsey <patsey.pavel@gmail.com>
2025-11-07 18:00:41 +0300

    Add medium 0056 solution in Python

---
+++ py/medium/0056/0056.py
@@ -0,0 +1,35 @@
+from operator import itemgetter
+
+
+class Solution:
+    def merge(self, intervals: list[list[int]]) -> list[list[int]]:
+        vals = sorted(intervals, key=itemgetter(0, 1))
+        i = 0
+        while i < len(vals) - 1:
+            a = vals[i]
+            b = vals[i + 1]
+            if is_intersect(a, b):
+                vals[i] = merge(a, b)
+                vals.pop(i + 1)
+            else:
+                i += 1
+        return vals
+
+
+def is_intersect(a: list, b: list) -> bool:
+    return a[1] >= b[0]
+
+
+def merge(a: list, b: list) -> list:
+    return [min(a[0], b[0]), max(a[1], b[1])]
+
+
+solution = Solution()
+assert solution.merge([[1, 3], [2, 6], [8, 10], [15, 18]]) == [
+    [1, 6],
+    [8, 10],
+    [15, 18],
+]
+assert solution.merge([[1, 4], [4, 5]]) == [[1, 5]]
+assert solution.merge([[4, 7], [1, 4]]) == [[1, 7]]
+assert solution.merge([[1, 4], [2, 3]]) == [[1, 4]]
