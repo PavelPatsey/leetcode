2944b97e0dadbef6d99a446286aa2c84343db38c
Pavel Patsey <patsey.pavel@gmail.com>
2025-11-09 17:22:04 +0300

    refactor

--- py/medium/0438/0438.py
+++ py/medium/0438/0438.py
@@ -3,17 +3,19 @@ from collections import Counter
 
 class Solution:
     def findAnagrams(self, s: str, p: str) -> list[int]:
-        counter_p = Counter(p)
-        print(counter_p)
+        if len(p) > len(s):
+            return []
+        reference_counter = Counter(p)
+        current_counter = Counter(s[0 : len(p) - 1])
         res = []
         for i in range(len(s) - len(p) + 1):
-            current_counter = Counter(s[i : i + len(p)])
-            if current_counter == counter_p:
+            current_counter[s[i + len(p) - 1]] += 1
+            if current_counter == reference_counter:
                 res.append(i)
-        print(f"{res=}")
+            current_counter[s[i]] -= 1
         return res
 
 
 solution = Solution()
 assert solution.findAnagrams("cbaebabacd", "abc") == [0, 6]
-assert solution.findAnagrams("abab", p="ab") == [0, 1, 2]
+assert solution.findAnagrams("abab", "ab") == [0, 1, 2]
