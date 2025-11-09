d48aa4105e53e6f4ca418079907b3bf3d60cfcb8
Pavel Patsey <patsey.pavel@gmail.com>
2025-11-09 14:45:12 +0300

    Add easy 3354 solution in Python

---
+++ py/easy/3354/3354.py
@@ -0,0 +1,23 @@
+class Solution:
+    def countValidSelections(self, nums: list[int]) -> int:
+        res = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                left = sum(nums[:i])
+                right = sum(nums[i + 1 :])
+                if left == right:
+                    res += 2
+                elif abs(left - right) == 1:
+                    res += 1
+                else:
+                    pass
+        return res
+
+
+solution = Solution()
+assert solution.countValidSelections([1, 0, 2, 0, 3]) == 2
+assert solution.countValidSelections([2, 3, 4, 0, 4, 1, 0]) == 0
+assert solution.countValidSelections([0, 2, 3, 1, 3, 1, 0, 0, 0, 0, 0, 0, 0, 10]) == 14
+
+
+assert solution.countValidSelections([1, 0, 0]) == 2
