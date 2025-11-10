8cfea650350c54bfdd30006d5f77d86e4dd9c846
Pavel Patsey <patsey.pavel@gmail.com>
2025-11-10 22:38:48 +0300

    refactor solution with while

--- py/medium/0443/0443.py
+++ py/medium/0443/0443.py
@@ -1,27 +1,20 @@
 class Solution:
     def compress(self, chars: list[str]) -> int:
-        prev = chars[0]
-        counter = 1
         l = 0
-        for i in range(1, len(chars)):
-            if chars[i] == prev:
-                counter += 1
-            else:
-                chars[l] = prev
-                l += 1
-                if counter > 1:
-                    for c in str(counter):
-                        chars[l] = c
-                        l += 1
-                counter = 1
-            prev = chars[i]
-        chars[l] = prev
-        l += 1
-        if counter > 1:
-            for c in str(counter):
-                chars[l] = c
-                l += 1
-        return l
+        r = 0
+        i = 0
+        while r < len(chars):
+            while r < len(chars) and chars[r] == chars[l]:
+                r += 1
+            chars[i] = chars[l]
+            i += 1
+            length = r - l
+            if length > 1:
+                for c in str(length):
+                    chars[i] = c
+                    i += 1
+            l = r
+        return i
 
 
 solution = Solution()
