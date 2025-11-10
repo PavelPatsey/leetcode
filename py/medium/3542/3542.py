60803497a2be8f78dae11e9c33529b145a82d66c
Pavel Patsey <patsey.pavel@gmail.com>
2025-11-10 17:46:20 +0300

    Add TLE solution

--- py/medium/3542/3542.py
+++ py/medium/3542/3542.py
@@ -1,7 +1,3 @@
-from collections import defaultdict
-from operator import itemgetter
-
-
 class Solution:
     def minOperations(self, nums: list[int]) -> int:
         res = min_operations(nums)
@@ -14,9 +10,8 @@ def min_operations(nums: list[int]) -> int:
     if 0 in nums:
         splitted_nums = split_by_zero(nums)
         return sum(min_operations(xs) for xs in splitted_nums)
-    groups = get_groups(nums)
-    max_key = max(groups.items(), key=itemgetter(1))[0]
-    new_nums = [0 if x == max_key else x for x in nums]
+    min_num = min(nums)
+    new_nums = [0 if x == min_num else x for x in nums]
     return 1 + min_operations(new_nums)
 
 
@@ -35,23 +30,6 @@ def split_by_zero(nums: list[int]) -> list[int]:
     return splitted
 
 
-def get_groups(nums: list[int]) -> defaultdict:
-    groups = defaultdict(int)
-    l = 0
-    r = 0
-    groups[nums[r]] += 1
-    for r in range(1, len(nums)):
-        if nums[r] != nums[l]:
-            groups[nums[r]] += 1
-            l = r
-    return groups
-
-
-assert dict(get_groups([0, 0, 0])) == {0: 1}
-assert dict(get_groups([0, 1, 2])) == {0: 1, 1: 1, 2: 1}
-assert dict(get_groups([1, 1, 1, 2, 2, 2, 2, 9, 9, 2])) == {1: 1, 2: 2, 9: 1}
-
-
 assert split_by_zero([0, 1, 2]) == [[1, 2]]
 assert split_by_zero([0, 2]) == [[2]]
 assert split_by_zero([2, 0]) == [[2]]
@@ -66,6 +44,7 @@ solution = Solution()
 assert solution.minOperations([0, 2]) == 1
 assert solution.minOperations([3, 1, 2, 1]) == 3
 assert solution.minOperations([1, 2, 1, 2, 1, 2]) == 4
+assert solution.minOperations([4, 3, 4, 6]) == 4
 
 # my_tests
 assert solution.minOperations([0]) == 0
