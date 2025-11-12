074cfeecac881f9e8df3263f05a29c7247ccf945
Pavel Patsey <patsey.pavel@gmail.com>
2025-11-12 13:43:34 +0300

    Add recursion solution

---
+++ py/easy/0206/0206_recursion.py
@@ -0,0 +1,49 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    def travers(self) -> list:
+        res = []
+        node = self
+        while node:
+            res.append(node.val)
+            node = node.next
+        return res
+
+
+class Solution:
+    def reverseList(self, head: ListNode | None) -> ListNode | None:
+        return revers_list(prev=None, node=head)
+
+
+def revers_list(prev: ListNode | None, node: ListNode | None) -> ListNode | None:
+    if node is None:
+        return None
+    if node.next is None:
+        node.next = prev
+        return node
+    next_node = node.next
+    node.next = prev
+    return revers_list(node, next_node)
+
+
+solution = Solution()
+
+node5 = ListNode(5)
+node4 = ListNode(4, node5)
+node3 = ListNode(3, node4)
+node2 = ListNode(2, node3)
+original_head = ListNode(1, node2)
+
+
+node1 = ListNode(1)
+node2 = ListNode(2, node1)
+node3 = ListNode(3, node2)
+node4 = ListNode(4, node3)
+reversed_head = ListNode(5, node4)
+
+assert original_head.travers() == [1, 2, 3, 4, 5]
+assert reversed_head.travers() == [5, 4, 3, 2, 1]
+expected_reversed_head = solution.reverseList(original_head)
+assert reversed_head.travers() == expected_reversed_head.travers()
