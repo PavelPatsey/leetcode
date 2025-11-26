8688e5a7b2ff06255de6e6f6aba98085d2a9a765
Pavel Patsey <patsey.pavel@gmail.com>
2025-11-26 23:31:44 +0300

    solve hard 2435

--- py/hard/2435/2435_cache.py
+++ py/hard/2435/2435_cache.py
@@ -1,24 +1,25 @@
 from typing import List
-from functools import cache
 
 
 class Solution:
     def numberOfPaths(self, grid: List[List[int]], k: int) -> int:
         ROWS = len(grid)
         COLS = len(grid[0])
-        DIRS = [(0, 1), (1, 0)]
         MOD = 10**9 + 7
+        cache = [[[-1] * k for _ in range(COLS)] for _ in range(ROWS)]
 
-        @cache
-        def dfs(r: int, c: int, total: int) -> int:
+        def dfs(r: int, c: int, remain: int) -> int:
             if r == ROWS or c == COLS:
                 return 0
-            new_total = (total + grid[r][c]) % k
+            new_remain = (remain + grid[r][c]) % k
             if (r, c) == (ROWS - 1, COLS - 1):
-                return 1 if new_total == 0 else 0
-            lst = [dfs(r + dr, c + dc, new_total) for dr, dc in DIRS]
-            sum_lst = sum(lst)
-            return sum_lst % MOD
+                return 1 if new_remain == 0 else 0
+            if cache[r][c][remain] != -1:
+                return cache[r][c][remain]
+            down = dfs(r + 1, c, new_remain)
+            right = dfs(r, c + 1, new_remain)
+            cache[r][c][remain] = (down + right) % MOD
+            return cache[r][c][remain]
 
         return dfs(0, 0, 0)
 
