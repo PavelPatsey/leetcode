1d19ab060bc3229894bb329af369aff1a4197457
Pavel Patsey <patsey.pavel@gmail.com>
2025-11-26 22:40:57 +0300

    refactor

--- py/hard/2435/2435_TLE.py
+++ py/hard/2435/2435_TLE.py
@@ -3,30 +3,24 @@ from typing import List
 
 class Solution:
     def numberOfPaths(self, grid: List[List[int]], k: int) -> int:
-        len_rows = len(grid)
-        len_cols = len(grid[0])
-        right = 0, 1
-        down = 1, 0
-        dirs = [right, down]
+        ROWS = len(grid)
+        COLS = len(grid[0])
+        DIRS = [(0, 1), (1, 0)]
         res = 0
 
-        def dfs(r: int, c: int, visited: set, total: int):
-            if not (0 <= r < len_rows and 0 <= c < len_cols):
+        def dfs(r: int, c: int, total: int):
+            if not (0 <= r < ROWS and 0 <= c < COLS):
                 return
-            if (r, c) in visited:
-                return
-            new_visited = visited | {(r, c)}
             new_total = (total + grid[r][c]) % k
-            if (r, c) == (len_rows - 1, len_cols - 1):
+            if (r, c) == (ROWS - 1, COLS - 1):
                 if new_total == 0:
                     nonlocal res
                     res += 1
                 return
+            for dr, dc in DIRS:
+                dfs(r + dr, c + dc, new_total)
 
-            for dr, dc in dirs:
-                dfs(r + dr, c + dc, new_visited, new_total)
-
-        dfs(0, 0, set(), 0)
+        dfs(0, 0, 0)
         return res
 
 
