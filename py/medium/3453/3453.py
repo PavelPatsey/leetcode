18ef6d33fe592ea38397f003a5e46ba66a1daae6
Pavel Patsey <patsey.pavel@gmail.com>
2026-01-13 22:03:49 +0300

    solve medium 3453

--- py/medium/3453/3453.py
+++ py/medium/3453/3453.py
@@ -3,52 +3,31 @@ EPSILON = 1e-05
 
 class Solution:
     def separateSquares(self, squares: list[list[int]]) -> float:
-        ys = set()
         squares = sorted(squares, key=lambda x: x[1])
         total = 0
-        y_left = float("inf")
-        y_right = -float("inf")
-        for _x, y, l in squares:
+        max_y = 0
+        for _, y, l in squares:
             total += l**2
-            y_left = min(y_left, y)
-            y_right = max(y_right, y + l)
-            ys.add(y)
-            ys.add(y + l)
+            max_y = max(max_y, y + l)
         ref = total / 2
-        for y_ref in sorted(ys):
-            half = calculate_bottom_half(squares, y_ref)
-            if are_equal(half, ref):
-                return y_ref
-        y_ref = (y_left + y_right) / 2
-        half = calculate_bottom_half(squares, y_ref)
-        while not are_equal(half, ref):
-            if half > ref:
-                y_right = y_ref
+        left, right = 0, max_y
+        while abs(left - right) > EPSILON:
+            mid = (left + right) / 2
+            if calculate_bottom_half(squares, mid) >= ref:
+                right = mid
             else:
-                y_left = y_ref
-            y_ref = (y_left + y_right) / 2
-            half = calculate_bottom_half(squares, y_ref)
-        return y_ref
+                left = mid
+        return left
 
 
 def calculate_bottom_half(squares: list[list[int]], y_ref: float) -> float:
     res = 0
     for x, y, l in squares:
-        if y_ref > y + l:
-            res += l**2
-        elif y <= y_ref <= y + l:
-            res += l * (y_ref - y)
-        elif y_ref < y:
-            pass
-        else:
-            raise Exception("invalid case!")
+        if y < y_ref:
+            res += l * min(y_ref - y, l)
     return res
 
 
-def are_equal(a: float, b: float, epsilon=EPSILON) -> bool:
-    return abs(a - b) < epsilon
-
-
 solution = Solution()
-assert are_equal(solution.separateSquares([[0, 0, 1], [2, 2, 1]]), 1) is True
-assert are_equal(solution.separateSquares([[0, 0, 2], [1, 1, 1]]), 1.16667) is True
+assert abs(solution.separateSquares([[0, 0, 1], [2, 2, 1]]) - 1) < EPSILON
+assert abs(solution.separateSquares([[0, 0, 2], [1, 1, 1]]) - 1.16667) < EPSILON
